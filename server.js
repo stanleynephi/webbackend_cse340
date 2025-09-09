e3bad860ca975e5a2f987d2830394bf852d1b790
stanleynephi <nul23001@byui.edu>
2025-09-09 12:43:48 +0000

    Week 01 assignment, application setup

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
@@ -9,11 +9,21 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expresslayouts = require("express-ejs-layouts")
+
+/**ejs template engine setup */
+app.set("view engine", "ejs")
+app.use(expresslayouts)
+app.set("layout", "./layout/layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
